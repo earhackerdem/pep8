f2f2ce47c27539ff35b7fac922364cbf5d9dfd58
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2026-06-02 19:57:24 -0600

    tipado de funciones y estructuras de datos en python

--- type-hints.py
+++ type-hints.py
@@ -14,3 +14,20 @@ print(f"Variable {otra_variable}, del tipo {type(otra_variable)}")
 otra_variable = 'Texto'
 
 user_id: int | None = None
+
+def suma_clara(a: int,b: int) -> int:
+    return a +b
+
+articles: list[dict] = [
+    {
+        "title":'Example'
+    },
+    {
+        'title': 'Example 2'
+    }
+]
+
+articles_dos: list[list][str] = [
+    ['lista','lista'],
+    ['lista','lista']
+]
